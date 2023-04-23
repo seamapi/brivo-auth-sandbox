691e078bb0c3bc2f97666a502ebef7585e3e291a
Dawn Ho <dawn.ho@gmail.com>
2023-04-22 22:56:07 -0700

    use axios

--- src/pages/api/initiate-brivo-oauth.ts
+++ src/pages/api/initiate-brivo-oauth.ts
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import axios from 'axios'
 
 type Data = {
 }
@@ -8,15 +9,19 @@ export default async (
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) => {
-  const response = await fetch('https://auth.brivo.com/oauth/authorize', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded', 
-      'Authorization': 'Basic OTYxNmQ3MWEtMzQ3MC00ZDcxLWEzNTktYWQ1Y2M4MTYyOTljOmxDWGZhMTNqUG9oUmk4blE5N1A4Y3U4STliajlqblVO',
-      'api-key': '6pu4sb9dccvevnkhzb7e66sc'
-    },
-    body: 'response_type=code&client_id=9616d71a-3470-4d71-a359-ad5cc816299c&state=abcd' 
-  })
+  const response = await axios.post('https://auth.brivo.com/oauth/authorize', 
+    'response_type=code&client_id=9616d71a-3470-4d71-a359-ad5cc816299c&state=abcd',
+    {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': 'Basic OTYxNmQ3MWEtMzQ3MC00ZDcxLWEzNTktYWQ1Y2M4MTYyOTljOmxDWGZhMTNqUG9oUmk4blE5N1A4Y3U4STliajlqblVO',
+        'api-key': '6pu4sb9dccvevnkhzb7e66sc'
+      }
+    }
+  )
 
-  res.status(200).json({ url: response.url })
+  res.status(200).json({
+    url: response.request._redirectable._options.href,
+    data: response.data,
+  })
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,4 @@
-import Image from 'next/image'
 import { useRouter } from 'next/router'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   const router = useRouter()
@@ -10,13 +6,12 @@ export default function Home() {
   const handleClick = async () => {
     const res = await fetch('/api/initiate-brivo-oauth')
     const data = await res.json()
-
     router.push(data.url)
   }
 
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex min-h-screen flex-col items-center justify-between p-24`}
     >
       <button onClick={handleClick}>Click</button>
     </main>
